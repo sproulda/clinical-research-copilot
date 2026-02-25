5ce452ed00f373a4c24f62cb581f25474720227d
sproulda <sprouldawson1@gmail.com>
2026-02-25 13:15:04 +0100

    Update settings for proper db connection

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,12 @@
 from pydantic_settings import BaseSettings
 
 
+# app/core/config.py
+
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/postgres"
+    DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/crc_db"  # <- updated
     DEBUG: bool = True
     LOG_LEVEL: str = "INFO"
 
     class Config:
         env_file = ".env"
-
-
-settings = Settings()
